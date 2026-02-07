d1d83bee33ebe72881e5be78aadbe3b24cb25683
masterj <masterj122517@163.com>
2026-02-07 10:50:45 +0800

    Update base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://masterj122517.github.io',
-  base: '/new_blog',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
